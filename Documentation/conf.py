19cbf6630eba20eddeb6ad220cb81ad264986614
Gerald Loß <gerald.loss@gmx.de>
2021-12-18 23:45:34 +0100

    Documentation corrections

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -26,7 +26,7 @@ author = 'Gerald Loß'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '2.3.0'
+release = '2.3'
 
 
 # -- General configuration ---------------------------------------------------
